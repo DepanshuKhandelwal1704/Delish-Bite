f87454d3aa33768d29d4f1aef158f3d361f2ebd9
DepanshuKhandelwal1704 <deepanshu.gv072@gmail.com>
2024-09-07 01:05:58 +0530

    Add files via upload

---
+++ Delish Bite/index.js
@@ -0,0 +1,23 @@
+let heroformcontainer= document.querySelector('.form-container')
+let form= document.querySelector('.formm')
+let button = document.querySelector('button')
+let submitt= document.querySelector('.submit')
+
+button.addEventListener('click',function(e){
+    e.preventDefault();
+    if (heroformcontainer.style.display === 'none' || heroformcontainer.style.display === '') {
+        heroformcontainer.style.display = 'block';
+    } else {
+        heroformcontainer.style.display = 'none';
+    }
+    
+})
+    form.addEventListener('submit', function(e){
+        e.preventDefault();
+        let formdata= new FormData(e.target);
+        let obj={};
+        FormData.forEach((val,key)=>{
+            obj[key]=val;
+        })
+        console.log('Form Submitted:', obj);
+})
